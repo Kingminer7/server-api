0e7c467370ecf2b2a149b96888846fcdcab069b8
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2024-08-28 01:01:08 -0400

    main class

---
+++ src/ServerAPI.cpp
@@ -0,0 +1,22 @@
+
+#include <Geode/Geode.hpp>
+#include "../include/ServerAPI.hpp"
+
+using namespace geode::prelude;
+
+ServerAPI * ServerAPI::instance = nullptr;
+
+void ServerAPI::setServerURL(std::string url) {
+    if (!url.ends_with("/")) url = url + "/";
+    this->server = url;
+    Mod::get()->setSettingValue<std::string>("server", url);
+}
+
+std::string ServerAPI::getServerURL() {
+    return this->server;
+}
+
+$on_mod(Loaded) {
+    std::string url = Mod::get()->getSettingValue<std::string>("server");
+    ServerAPI::get()->setServerURL(url);
+}
