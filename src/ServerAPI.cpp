f19174782ae99634af7368ae6f428a069eb6552d
Kingminer7 <km7@km7dev.me>
2024-08-28 22:16:59 -0400

    Other mods should probably handle saving server

--- src/ServerAPI.cpp
+++ src/ServerAPI.cpp
@@ -9,17 +9,8 @@ ServerAPI * ServerAPI::instance = nullptr;
 void ServerAPI::setServerURL(std::string url) {
     if (!url.ends_with("/")) url = url + "/";
     this->server = url;
-    Mod::get()->getSavedValue<std::string>("server", url);
 }
 
 std::string ServerAPI::getServerURL() {
     return this->server;
-}
-
-$on_mod(Loaded) {
-    std::string url = Mod::get()->getSavedValue<std::string>("server");
-    if (url == "") {
-        url = "https://www.boomlings.com/database/";
-    }
-    ServerAPI::get()->setServerURL(url);
 }
