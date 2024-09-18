c22a67b113c48f18304e797c9d2b90250cd04c26
Kingminer7 <km7@km7dev.me>
2024-09-18 18:42:10 +0000

    man why cant it just give the line numbers when codespaces compiles

--- src/ServerAPI.cpp
+++ src/ServerAPI.cpp
@@ -6,7 +6,7 @@ using namespace geode::prelude;
 
 ServerAPI * ServerAPI::instance = nullptr;
 
-int ServerAPI::setServerURL(std::string url, int priority) {
+int ServerAPI::setURL(std::string url, int priority) {
     if (!url.ends_with("/")) url = url + "/";
     // <id, <url, prio>>
     this->overrides[this->nextId] = std::pair(url, priority);
@@ -14,15 +14,52 @@ int ServerAPI::setServerURL(std::string url, int priority) {
     return this->nextId - 1;
 }
 
+void ServerAPI::updateURLAndPrio(int id, std::string url, int priority) {
+    if (!url.ends_with("/")) url = url + "/";
+    // <id, <url, prio>>
+    this->overrides[id] = std::pair(url, priority);
+}
+
+void ServerAPI::updateURL(int id, std::string url) {
+    if (!url.ends_with("/")) url = url + "/";
+    // <id, <url, prio>>
+    int priority = this->overrides[id].second;
+    this->overrides[id] = std::pair(url, priority);
+}
+
+void ServerAPI::updatePrio(int id, int priority) {
+    // <id, <url, prio>>
+    std::string url = this->overrides[id].first;
+    this->overrides[id] = std::pair(url, priority);
+}
+
+std::string ServerAPI::getURLById(int id) {
+    for (const auto& [uid, urlPrio] : overrides) {
+        if (id == uid) {
+            return urlPrio.first;
+        }
+    }
+    return nullptr;
+}
+
+int ServerAPI::getPrioById(int id) {
+    for (const auto& [uid, urlPrio] : overrides) {
+        if (id == uid) {
+            return urlPrio.second;
+        }
+    }
+    return 0;
+}
+
 std::string ServerAPI::getServerURL() {
-    auto highestPrioUrl = std::make_pair("https://www.boomlings.com/database/", INT_MIN);
+    std::pair<std::string, int> highestPrioUrl = std::make_pair("https://www.boomlings.com/database/", INT_MIN);
     
     for (const auto& [id, urlPrio] : overrides) {
         const auto& [url, prio] = urlPrio;
         if (prio > highestPrioUrl.second) {
-            return url;
+            highestPrioUrl = urlPrio;
         }
     }
     
-    return "https://www.boomlings.com/database/";
+    return highestPrioUrl.first;
 }
